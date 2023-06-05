538f5a0ae0af7e7b52b70b2f3a63546329f07d7d
jbxamora <jbxamora@icloud.com>
2023-06-05 03:58:18 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,58 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const navigation = [
-	{ name: "Projects", href: "/projects" },
-	{ name: "Contact", href: "/contact" },
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
 ];
 
 export default function Home() {
-	return (
-<>
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        kronos
-      </h1>
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+        <nav className="my-16 animate-fade-in">
+          <ul className="flex items-center justify-center gap-4">
+            {navigation.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <Particles
+          className="absolute inset-0 -z-10 animate-fade-in"
+          quantity={100}
+        />
+        <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+          kronos
+        </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          Hi, my name is Jorge, I build timeless websites and open source
-          solutions for small businesses and startups.
-          <br />
-          Im currently working on{" "}
-          <Link
-            target="_blank"
-            href="https://photoport.vercel.app"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            photoport,
-          </Link>{" "}
-          an image gallery meant to be used as a portfolio for photographers.
-        </h2>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="my-16 text-center animate-fade-in">
+          <h2 className="text-sm text-zinc-500 ">
+            Hi, my name is Jorge, I build timeless websites and open source
+            solutions for small businesses and startups.
+            <br />
+            Im currently working on{" "}
+            <Link
+              target="_blank"
+              href="https://photoport.vercel.app"
+              className="underline duration-500 hover:text-zinc-300"
+            >
+              photoport,
+            </Link>{" "}
+            an image gallery meant to be used as a portfolio for photographers.
+          </h2>
+        </div>
       </div>
-    </div>
-<Analytics />
-</>
+      <Analytics />
+    </>
   );
 }
