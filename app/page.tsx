6a5274a6fe14d9429913509159b34a2f68b8794c
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-06-05 03:29:57 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import { Analytics } from '@vercel/analytics/react';
 
 const navigation = [
 	{ name: "Projects", href: "/projects" },
@@ -9,6 +10,7 @@ const navigation = [
 
 export default function Home() {
 	return (
+<>
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
@@ -50,5 +52,7 @@ export default function Home() {
         </h2>
       </div>
     </div>
+<Analytics />
+</>
   );
 }
