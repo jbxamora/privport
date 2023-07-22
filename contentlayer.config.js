86ca4f6937ffaed4835b695298cc167ec1f61139
jbxamora <jbxamora@icloud.com>
2023-07-22 16:09:33 -0700

    remove comment

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -74,8 +74,6 @@ export default makeSource({
 				{
 					theme: "github-dark",
 					onVisitLine(node) {
-						// Prevent lines from collapsing in `display: grid` mode, and allow empty
-						// lines to be copy/pasted
 						if (node.children.length === 0) {
 							node.children = [{ type: "text", value: " " }];
 						}
